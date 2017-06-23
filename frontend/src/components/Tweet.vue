3f9462f4b7c98b93a577e4dbe4e0cf833855070d
arnaud <arnaud.frossard@gmail.com>
2017-06-23 11:49:56 +0200

    Implement relative date with momentJS

--- frontend/src/components/Tweet.vue
+++ frontend/src/components/Tweet.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="tweet">
     <div>
-      <strong>{{ tweet.auteur.prenom }} {{ tweet.auteur.nom }}</strong> <span class="handle">@{{ tweet.auteur.handle }}</span>
+      <strong>{{ tweet.auteur.prenom }} {{ tweet.auteur.nom }}</strong>
+      <span class="handle">@{{ tweet.auteur.handle }}</span> - {{ moment(tweet.date).fromNow() }}
     </div>
     <div>
       {{ tweet.contenu }}
@@ -19,12 +20,23 @@
 
 <script>
 import 'vue-awesome/icons'
+import moment from 'moment'
 import Icon from 'vue-awesome/components/Icon'
 export default {
   components: {Icon},
   name: 'tweet',
-  props: ['tweet']
+  props: ['tweet'],
+  methods: {
+    moment: function (date) {
+      return moment(date)
+    }
+  },
+
+  created () {
+    moment.locale('fr')
+  }
 }
+
 </script>
 
 <style scoped>
